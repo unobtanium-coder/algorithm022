44d62f7ee62ba53e417744cddf76b31d77f3a759
yhd <unobtanium@vip.qq.com>
2021-01-18 23:59:20 +0800

    Week 04 assignments

--- Week_04/W04#0045_jump.cpp
+++ Week_04/W04#0045_jump.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int end = 0;
+        int end = 0;  //最远可及处
         int currend;
         int size = nums.size();
         int step = 0;
-        for (int i = 0; i < size; ) {
+        for (int i = 0; (i < size) && (i <= end); ) {
             if (end >= size - 1)  return step;
             currend = end;
             while (i <= currend) {
@@ -16,6 +16,6 @@ public:
             // if (end >= size - 1)  return step;
         }
 
-        return step;
+        return -1; //不可及
     }
 };
